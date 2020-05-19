28fe1e1efae20e9aad8a37a95fdfbc853f571057
Priyanshi Sharma <priyanshi417@gmail.com>
2020-05-19 23:56:35 +0530

    Problem 28

---
+++ 19May_Sum_Root_to_Leaf.cpp
@@ -0,0 +1,40 @@
+// Problem 28
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumrl = 0;
+    void sum(TreeNode* root, int num)
+    {
+        if(root->left == NULL && root->right == NULL)
+        {
+            num = num*10 + root->val;
+            sumrl+=num;
+            return;
+        }
+        if(root->left)
+            sum(root->left, num*10 + root->val);
+        
+        if(root->right)
+            sum(root->right, num*10 + root->val);
+
+    }
+    int sumNumbers(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        
+        sum(root, 0);
+        
+        return sumrl;
+    }
+};
